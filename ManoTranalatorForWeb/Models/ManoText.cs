88f4396f4dbf165c69918065f9c4aadcfb50b2f8
poketorena <poketorena@users.noreply.github.com>
2018-05-31 18:03:27 +0900

    モデルを追加

---
+++ ManoTranalatorForWeb/Models/ManoText.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManoTranalatorForWeb.Models
+{
+    public class ManoText
+    {
+        public int Id { get; set; }
+        public string InputText { get; set; }
+        public string OutputText { get; set; }
+    }
+}
